c84686b3ee8ffa50c8b0712386e5288d1a348993
Ellis <ellis@s-team.com.tw>
2016-04-29 15:18:10 +0800

    message resource handler prototype created.
    placed under org/syachiku/madao/messenger/resources/MessageResource.java

---
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -0,0 +1,17 @@
+package org.syachiku.madao.messenger.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessages(){
+		return "Hello Wolrd!";
+	}
+}
