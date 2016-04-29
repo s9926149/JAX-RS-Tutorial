a6a1136c1fd33b5a057ea42256ac446f0e95a676
Ellis <ellis@s-team.com.tw>
2016-04-29 17:15:24 +0800

    MessageService modified, now work with DatabaseClass, not hardcoded messages anymore.

--- src/main/java/org/syachiku/madao/messenger/service/MessageService.java
+++ src/main/java/org/syachiku/madao/messenger/service/MessageService.java
@@ -2,18 +2,38 @@ package org.syachiku.madao.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.syachiku.madao.messenger.database.DatabaseClass;
 import org.syachiku.madao.messenger.model.Message;
 
 public class MessageService {
-
+	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
 	public List<Message> getAllMessages(){
-		Message m1 = new Message(1L, "Hello World!", "Madao");
-		Message m2 = new Message(2L, "Hello Jersey!", "Madao");
-		
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id){
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message){
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message){
+		if (message.getId() <= 0){
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
 	}
 }
