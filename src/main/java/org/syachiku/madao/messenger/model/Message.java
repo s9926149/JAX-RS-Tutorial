ae692d4108abcc709cca5ac804e03b495a367e42
Ellis <ellis@s-team.com.tw>
2016-04-29 15:36:59 +0800

    Message added a constructor.

--- src/main/java/org/syachiku/madao/messenger/model/Message.java
+++ src/main/java/org/syachiku/madao/messenger/model/Message.java
@@ -9,6 +9,13 @@ public class Message {
 	private Date created;
 	private String author;
 	
+	public Message(long id, String message, String author) {
+		this.id = id;
+		this.message = message;
+		this.author = author;
+		this.created = new Date();
+	}
+	
 	public long getId() {
 		return id;
 	}
