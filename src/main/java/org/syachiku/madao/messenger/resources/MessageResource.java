3fd6b5908b2a07c4a47158cda340aba1208d0e5e
Ellis <ellis@s-team.com.tw>
2016-04-29 16:00:21 +0800

    MessageResource modified, return XML from messageService instead of TEXT hello world.

--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -1,17 +1,23 @@
 package org.syachiku.madao.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.syachiku.madao.messenger.model.Message;
+import org.syachiku.madao.messenger.service.MessageService;
+
 @Path("/messages")
 public class MessageResource {
 	
+	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages(){
-		return "Hello Wolrd!";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages(){
+		return messageService.getAllMessages();
 	}
 }
