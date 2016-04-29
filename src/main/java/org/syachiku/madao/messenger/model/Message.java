d7a7b9c0934513fdc891fd810aa8a4922b394355
Ellis <ellis@s-team.com.tw>
2016-04-29 15:31:47 +0800

    Message model created.
    placed under org/syachiku/madao/messenger/model/

---
+++ src/main/java/org/syachiku/madao/messenger/model/Message.java
@@ -0,0 +1,37 @@
+package org.syachiku.madao.messenger.model;
+
+import java.util.Date;
+
+public class Message {
+	
+	private long id;
+	private String message;
+	private Date created;
+	private String author;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+}
