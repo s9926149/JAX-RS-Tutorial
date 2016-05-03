8ec0b836625b08aada7b832d84345abf620a9249
Ellis <ellis@s-team.com.tw>
2016-05-03 15:06:27 +0800

    Implemented ProfileResource and ProfileService.

---
+++ src/main/java/org/syachiku/madao/messenger/service/ProfileService.java
@@ -0,0 +1,45 @@
+package org.syachiku.madao.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.syachiku.madao.messenger.database.DatabaseClass;
+import org.syachiku.madao.messenger.model.Profile;
+
+
+
+public class ProfileService {
+	
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	public ProfileService(){
+		profiles.put("Madao", new Profile(1L, "Madao", "Madao", "Hasegawa"));
+	}
+	
+	public List<Profile> getAllProfiles(){
+		return new ArrayList<Profile>(profiles.values());
+	}
+	
+	public Profile getProfile(String profileName){
+		return profiles.get(profileName);
+	}
+	
+	public Profile addProfile(Profile profile){
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile){
+		if (profile.getProfileName().isEmpty()){
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile removeProfile(String profileName){
+		return profiles.remove(profileName);
+	}
+}
