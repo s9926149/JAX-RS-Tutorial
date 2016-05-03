24b2eb7a65c6e150358522a575ef33efbb922d8a
Ellis <ellis@s-team.com.tw>
2016-05-03 13:58:28 +0800

    MessageResource added PUT and DELETE, Consumes and Produces annotation moved to class declaration.

--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.syachiku.madao.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +16,38 @@ import org.syachiku.madao.messenger.model.Message;
 import org.syachiku.madao.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id){
 		return messageService.getMessage(id);
 	}
 	
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message){
 		return messageService.addMessage(message);
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message){
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id){
+		messageService.removeMessage(id);
+	}
 }
