33bcbff9fa60de6702ebf66ac314151b7e857b07
Ellis <ellis@s-team.com.tw>
2016-05-03 13:34:56 +0800

    MessageResource added POST method to accept receive and add create new messages.

--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.syachiku.madao.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -28,4 +30,12 @@ public class MessageResource {
 	public Message getMessage(@PathParam("messageId") long id){
 		return messageService.getMessage(id);
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
 }
