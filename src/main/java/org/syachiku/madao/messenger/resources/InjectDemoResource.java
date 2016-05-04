49fcfe3baff670afbd5cd585c90bed8770dc58c6
Ellis <ellis@s-team.com.tw>
2016-05-04 11:43:51 +0800

    Added MessageFilterBean to replace params in MessageResource
    InjectDemoResource also added context params demo.

--- src/main/java/org/syachiku/madao/messenger/resources/InjectDemoResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -21,5 +24,13 @@ public class InjectDemoResource {
 										   @CookieParam("name") String cookie){
 		
 		return "Matrix Param: " + martrixParam + ", Header: " + header + ", Cookie: " + cookie;
+	}
+	
+	@GET
+	@Path("/context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers){
+		String path = uriInfo.getAbsolutePath().toString();
+		String cookie = headers.getCookies().toString();
+		return "Path: " + path + ", Cookie: " + cookie;
 	}
 }
--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package org.syachiku.madao.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,10 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.syachiku.madao.messenger.model.Message;
+import org.syachiku.madao.messenger.resources.beans.MessageFilterBean;
 import org.syachiku.madao.messenger.service.MessageService;
 
 @Path("/messages")
@@ -24,15 +25,13 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	public List<Message> getMessages(@QueryParam("year") int year,
-									 @QueryParam("start") int start,
-									 @QueryParam("size") int size){
-		if (year > 0) {
-			return messageService.getAllMessagesForYear(year);
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean){
+		if (filterBean.getYear() > 0) {
+			return messageService.getAllMessagesForYear(filterBean.getYear());
 		}
 		
-		if (start >= 0 && size > 0){
-			return messageService.getAllMessagesPaginated(start, size);
+		if (filterBean.getStart() >= 0 && filterBean.getSize() > 0){
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
 		}
 		
 		return messageService.getAllMessages();
