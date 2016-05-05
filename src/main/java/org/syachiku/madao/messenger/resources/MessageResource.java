f2f4c040ae8c0e6955125d97e7853421bf9344fd
Ellis <ellis@s-team.com.tw>
2016-05-05 10:40:15 +0800

    POST method for MessageResource rewrited, now send back lacation in headers.

--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -1,5 +1,7 @@
 package org.syachiku.madao.messenger.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,9 +13,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import org.syachiku.madao.messenger.model.Message;
 import org.syachiku.madao.messenger.resources.beans.MessageFilterBean;
@@ -46,9 +49,11 @@ public class MessageResource {
 	}
 	
 	@POST
-	public Response addMessage(Message message){
+	public Response addMessage(Message message, @Context UriInfo uriInfo) throws URISyntaxException{
 		Message newMessage = messageService.addMessage(message);
-		return Response.status(Status.CREATED).entity(newMessage).build();
+		String newId = String.valueOf(newMessage.getId());
+		URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
+		return Response.created(uri).entity(newMessage).build();
 		
 		// First version of addMessage implementation
 		//return messageService.addMessage(message);
