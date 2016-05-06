b0296ee757fac93d57de3f901cc21ef42a31acfe
Ellis <ellis@s-team.com.tw>
2016-05-06 10:53:03 +0800

    Separated MessageResource GET method into two to practice content negotiation.

--- src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
+++ src/main/java/org/syachiku/madao/messenger/resources/MessageResource.java
@@ -30,7 +30,26 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean){
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getJsonMessages(@BeanParam MessageFilterBean filterBean){
+		System.out.println("JSON method called");
+		
+		if (filterBean.getYear() > 0) {
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		}
+		
+		if (filterBean.getStart() >= 0 && filterBean.getSize() > 0){
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		}
+		
+		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public List<Message> getXmlMessages(@BeanParam MessageFilterBean filterBean){
+		System.out.println("XML method called");
+		
 		if (filterBean.getYear() > 0) {
 			return messageService.getAllMessagesForYear(filterBean.getYear());
 		}
